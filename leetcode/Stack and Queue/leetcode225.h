1736a17d2f5b8f0496dc674d2343d4b0abdf2b7e
Peixiao Zhao <u6935249@anu.edu.au>
2021-06-15 19:56:18 +0800

    Stack & Queue
    2021.6.15

---
+++ leetcode/Stack and Queue/leetcode225.h
@@ -0,0 +1,60 @@
+//
+// Created by Peixiao on 2021/6/15.
+//
+
+#ifndef STACK_AND_QUEUE_LEETCODE225_H
+#define STACK_AND_QUEUE_LEETCODE225_H
+
+#endif //STACK_AND_QUEUE_LEETCODE225_H
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class MyStack {
+public:
+    queue<int> q1, q2;
+    /** Initialize your data structure here. */
+    MyStack() {
+
+    }
+
+    /** Push element x onto stack. */
+    void push(int x) {
+        q1.push(x);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int val;
+        while (!q1.empty()) {
+            if (q1.size() == 1) {
+                val = q1.front();
+            }else{
+                q2.push(q1.front());
+            }
+            q1.pop();
+        }
+        q2.swap(q1);
+        return val;
+    }
+
+    /** Get the top element. */
+    int top() {
+        int val;
+        while (!q1.empty()) {
+            if (q1.size() == 1) {
+                val = q1.front();
+            }
+            q2.push(q1.front());
+            q1.pop();
+        }
+        q2.swap(q1);
+        return val;
+    }
+
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return q1.empty();
+    }
+};
