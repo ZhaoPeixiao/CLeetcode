9a6374507ed9ffea2d48351fd77ac95484775c52
Peixiao Zhao <u6935249@anu.edu.au>
2021-06-22 19:53:53 +0800

    HW exercise
    2021.6.22

--- leetcode/HW/main.cpp
+++ leetcode/HW/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <iostream>
-#include <limits>
-#include "Q3.h"
+#include <memory>
+#include <string.h>
 using namespace std;
 
+
+/*
+ * upSrt2: 100
+ * upSrt2: 200
+ *
+ * 0 0
+ *
+ * 877
+ *
+ * bar foo b_bar
+ *
+ * 4132
+ *
+ * 12
+ *
+ * Sub BaseDisplay
+ */
+
+
 int main() {
-//    std::cout << "Hello, World!" << std::endl;
-//    return 0;
-    vector<string> str = {"A", "|-B", "|-|-C", "|-|-D", "|-|-E", "|-|-|-F", "|-lib32"};
-    vector<string> res = DelAllDirectorys(str);
-    for (int i = 0; i < res.size(); i++) {
-        cout << res[i] << endl;
-    }
+
+    return 0;
 }
